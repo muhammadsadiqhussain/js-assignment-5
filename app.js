6a96e42c933c4e9bd393032c9dea1ceda3a660ef
muhammadsadiqhussain <sh2453621@gmail.com>
2025-07-20 20:28:49 +0500

    complete Q 11

---
+++ app.js
@@ -0,0 +1,135 @@
+
+// let a = 5;
+// let b = 5;
+// let result = a + b;
+// console.log(result)
+
+// let a = 5;
+// let b = 5;
+// let result = a - b;
+// console.log(result)
+
+// let a = 5;
+// let b = 5;
+// let result = a * b;
+// console.log(result)
+
+// let a = 5;
+// let b = 5;
+// let result = a / b;
+// console.log(result)
+
+// let a;
+// document.write(`value after variable declaration is: ${a} <br>`)
+
+// a = 5;
+// document.write(`value is: ${a} <br>`)
+
+// a++;
+// document.write(`after increment: ${a} <br>`)
+
+// a += 7;
+// document.write(`value after add: ${a} <br>`)
+
+// a--;
+// document.write(`after decrement: ${a} <br>`)
+
+// // a /= 3;
+// document.write(`reminder: ${a % 3} <br>`)
+
+
+// let movieTicket = 600;
+
+// let costumerBuyTicket = 5;
+
+// let result = movieTicket * costumerBuyTicket;
+
+// document.write(`total cost to buy 5 tickets to a movie is: ${result}`)
+
+// let num = 5;
+// let num1 = 1;
+// let num2 = 2;
+// let num3 = 3;
+// let num4 = 4;
+// let num5 = 5;
+// let num6 = 6;
+// let num7 = 7;
+// let num8 = 8;
+// let num9 = 9;
+// let num10 = 10;
+
+// let result = num * num1;
+// let result2 = num * num2;
+// let result3 = num * num3;
+// let result4 = num * num4;
+// let result5 = num * num5;
+// let result6 = num * num6;
+// let result7 = num * num7;
+// let result8 = num * num8;
+// let result9 = num * num9;
+// let result10 = num * num10;
+
+// document.write(`<h1>Table of  ${num} </h1>`)
+// document.write(num + " X " + num1 + " = " + result, '<br>') 
+// document.write(num + " X " + num2 + " = " + result2, '<br>' )
+// document.write(num + " X " + num3 + " = " + result3, '<br>' )
+// document.write(num + " X " + num4 + " = " + result4, '<br>' )
+// document.write(num + " X " + num5 + " = " + result5, '<br>' )
+// document.write(num + " X " + num6 + " = " + result6, '<br>' )
+// document.write(num + " X " + num7 + " = " + result7, '<br>' )
+// document.write(num + " X " + num8 + " = " + result8, '<br>' )
+// document.write(num + " X " + num9 + " = " + result9, '<br>' )
+// document.write(num + " X " + num10 + " = " + result10, '<br>' )
+
+// let celciusTemp = 25
+
+// Ftemp = (celciusTemp * 9 / 5) + 32
+
+// document.write(celciusTemp + "C  is  F " + Ftemp, "<br>")
+
+// fahrenheitTemp = 77 
+
+// Ctemp = (fahrenheitTemp - 32) * 5 / 9
+
+// document.write(fahrenheitTemp + "F  is  C " + Ctemp)
+
+// let product1Price = 650
+// let product2Price = 100
+// let shipCharge = 100
+
+// let orderQuantity1 = 3
+// let orderQuantity2 = 7
+
+// let grossPurchase1 = orderQuantity1 * product1Price
+// let grossPurchase2 = orderQuantity2 * product2Price
+
+// document.write("Total  ",grossPurchase1 + grossPurchase2 + shipCharge)
+
+// let totalMarks = 980
+// let obtainMarks = 804
+
+// percentage = obtainMarks / totalMarks * 100
+
+// document.write(percentage)
+
+// let usDollar = 10
+// let saudiRiyal = 25 
+
+// let usDollarRate = 104.80
+// let saudiRiyalRate = 28
+
+// let UStoPKR = usDollar * usDollarRate
+// let saudiRiyaltoPKR = saudiRiyal * saudiRiyalRate
+
+// document.write(UStoPKR + saudiRiyaltoPKR)
+
+// let number = 100
+
+// let result = ((number + 5) * 10 ) / 2
+
+// document.write(result)
+
+// let currentYear = 2025
+// let birthYear = 2000
+// let age = currentYear - birthYear
+// document.write(age)
